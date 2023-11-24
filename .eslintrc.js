615ac765c2fd48f969bf2c9725cacb8748d82a43
Joshua Tazman Reinier <jreinier@oberlin.edu>
2023-11-23 23:07:57 -0700

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,18 @@
 module.exports = {
   root: true,
   extends: [
-    '@remix-run/eslint-config',
-    '@remix-run/eslint-config/node',
-    '@remix-run/eslint-config/jest-testing-library',
-    'prettier',
+    "@remix-run/eslint-config",
+    "@remix-run/eslint-config/node",
+    "@remix-run/eslint-config/jest-testing-library",
+    "prettier",
   ],
   env: {
-    'cypress/globals': true,
+    "cypress/globals": true,
   },
-  plugins: ['cypress'],
+  rules: {
+    "no-unused-vars": "off",
+  },
+  plugins: ["cypress"],
   // we're using vitest which has a very similar API to jest
   // (so the linting plugins work nicely), but it means we have to explicitly
   // set the jest version.
@@ -19,5 +22,5 @@ module.exports = {
       version: 28,
     },
   },
-  ignorePatterns: 'public',
-}
+  ignorePatterns: "public",
+};
--- app/components/PDF.tsx
+++ app/components/PDF.tsx
@@ -1,31 +1,30 @@
-import { icons } from '~/assets/assets'
+import { icons } from "~/assets/assets";
 
 export default function PDF({
   src,
   className,
-  dark = true,
 }: {
-  src: string
-  className?: string
-  dark?: boolean
+  src: string;
+  className?: string;
 }) {
   return (
     <div
       className={
-        'relative mx-auto my-2 h-[calc(100vh-200px)] w-full' + (className || '')
+        "relative mx-auto my-2 h-[calc(100vh-200px)] w-full" + (className || "")
       }
     >
-      <button className='absolute right-0 top-0 z-40 m-2 rounded border border-white bg-black p-1'>
-        <a href={src} target='_blank'>
+      <button className="absolute right-0 top-0 z-40 m-2 rounded border border-white bg-black p-1">
+        <a href={src} target="_blank">
           <img src={icons.fullScreen} height={16} width={16} />
         </a>
       </button>
       <object
         data={src}
-        type='application/pdf'
-        width='100%'
-        height='100%'
+        type="application/pdf"
+        width="100%"
+        height="100%"
+        className="invert"
       ></object>
     </div>
-  )
+  );
 }
